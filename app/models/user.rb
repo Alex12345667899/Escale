3c52eca8ca1ce2b656fc293f85cd8849e1c07486
Alexandre Guilmet <alexandre.guilmet@kedgebs.com>
2024-08-20 16:18:16 +0200

    c fé

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :trips, :reviews, :bookmarks, dependent: :destroy
+  has_many :trips, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 end
