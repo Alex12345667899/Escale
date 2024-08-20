333047e91f5fdda42d452c9d2e2fd2c4bbd9a067
vincent gannier <vincent.gannier@gmail.com>
2024-08-20 15:54:01 +0200

    set footprint

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  #before_save :set_total_duration
+  before_save :set_total_duration, :set_footprint
 
   private
 
@@ -8,5 +8,10 @@ class Trip < ApplicationRecord
   #   journey = (end_date - start_date).to_i
   #   full_duration = journey * self.offer.duration
   #   self.total_duration = full_duration
+  # end
+
+  # def set_footprint
+  #   total_footprint = 0
+  #   self.footprint = total_footprint
   # end
 end
