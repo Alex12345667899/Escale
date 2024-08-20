56413fadfa473f56812c5b22caf66ba6fe482552
Alexandre Guilmet <alexandre.guilmet@kedgebs.com>
2024-08-20 15:52:41 +0200

    init trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,25 @@
 class TripsController < ApplicationController
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.save
+    redirect_to trip_path(@trip)
+  end
+
+  # def destroy
+  #     @trip = Trip.find(params[:id])
+  #     @trip.destroy
+  #     redirect_to trips_path, status: :see_other
+  # end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title, :description)
+  end
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  has_many :steps, :reviews, :bookmarks, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
+  #validates :footprint, presence: true
+
 end
