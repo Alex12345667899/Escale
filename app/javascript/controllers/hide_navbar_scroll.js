2434b8090ade324e91a085198ce879a013d4abc1
lucas maillet <lucas.maillet9@gmail.com>
2024-08-22 15:33:01 +0200

    mon premier controller JS

--- app/javascript/controllers/hide_navbar_scroll.js
+++ app/javascript/controllers/hide_navbar_scroll.js
@@ -1,10 +1,10 @@
-const prevScrollpos = window.scrollY;
+let prevScrollpos = window.scrollY;
 window.onscroll = function() {
   const currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar d-flex justify-content-between align-items-center").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar d-flex justify-content-between align-items-center").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
