4feb538dfd6f0a0751fa30efdc6da0fc5bd2dcae
Alexandre Guilmet <alexandre.guilmet@kedgebs.com>
2024-08-28 16:00:19 +0200

    reviews back

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,18 @@
 class ReviewsController < ApplicationController
+  before_action :set_trip, only: %i[create]
+
+  def new
+    @review = Review.new
+    @bookmark = Bookmark.find(params[:bookmark_id])
+  end
+
   def create
     @review = Review.new(review_params)
+    @review.trip = @trip
     @review.user = current_user
     if @review.save
       flash[:success] = "Review was successfully created !"
-      redirect_to trip_path
+      redirect_to trip_path(@trip)
     else
       render :new, status: :unprocessable_entity
     end
@@ -12,8 +20,11 @@ class ReviewsController < ApplicationController
 
   private
 
+  def set_trip
+    @trip = Bookmark.find(params[:bookmark_id]).trip
+  end
+
   def review_params
     params.require(:review).permit(:rating, :content)
   end
-  
 end
