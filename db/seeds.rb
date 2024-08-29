0fe3414ce8e6991acf525e8799969e31e81632e9
Alexandre Guilmet <alexandre.guilmet@kedgebs.com>
2024-08-29 17:28:22 +0200

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,22 +53,9 @@ trip4.photo.attach(io: file4, filename: "gourmet_scenic.png", content_type: "ima
 trip4.save
 
 trip5 = Trip.new(title: "Vibrant Bordeaux to Valencia Expedition", description: "Travel from Bordeaux to Valencia with stops in Lisbon, Seville, and Granada, experiencing the rich history and vibrant culture of the Iberian Peninsula.", user: User.first, category: ["Beach Escapes", "Mountain Gateaways", "Rural Retreats"])
-file5 = URI.open("IMAGE_URL")
+file5 = URI.open("https://www.guide-bordeaux-gironde.com/_bibli/articlesPage/167/images/adobestock-saiko3p-bordeaux-vue-du-ciel.jpg?v=ficheArticle&width=572&height=275&pixelRatio=2.6250")
 trip5.photo.attach(io: file5, filename: "valencia_expedition.png", content_type: "image/png")
 trip5.save
-
-
-# trip_attributes = [
-  # { title: "Enchanting Bordeaux to Berlin Adventure", description: "Travel from Bordeaux to Berlin, passing through Paris, Brussels, and Amsterdam, exploring iconic landmarks, famous cuisine, and historical sites.", user: User.first, category: "Capital Cities" },
-  # { title: "Scenic Bordeaux to Nice Getaway", description: "Journey from Bordeaux to Nice via Madrid and Barcelona, experiencing vibrant Spanish culture and the beauty of the French Riviera.", user: User.first, category: ["Beach Escapes", "Rural Retreats"] },
-  # { title: "Cultural Odyssey: Bordeaux to Prague", description: "Begin in Bordeaux and travel through Geneva, Milan, Venice, and Vienna before reaching Prague, enjoying stunning architecture and rich history.", user: User.first, category: ["Capital Cities", "Mountain Gateaways", "Rural Retreats"] },
-  # { title: "Gourmet & Scenic Bordeaux to Munich Journey", description: "Start in Bordeaux, visit Lyon and Zurich, and conclude your trip in Munich, exploring culinary delights, lakeside views, and historic sites.", user: User.first, category: "Capital Cities" },
-  # { title: "Vibrant Bordeaux to Valencia Expedition", description: "Travel from Bordeaux to Valencia with stops in Lisbon, Seville, and Granada, experiencing the rich history and vibrant culture of the Iberian Peninsula.", user: User.first, category: ["Beach Escapes", "Mountain Gateaways", "Rural Retreats"] }
-# ]
-
-# trip_attributes.each do |trip_attribute|
-#   Trip.create!(trip_attribute)
-# end
 
 puts "#{Trip.count} trips created"
 
@@ -84,12 +71,12 @@ Step.destroy_all
 puts "all steps were destroyed"
 
 step1 = Step.new(title: "Bordeaux, France", content: "Discover the charm of Bordeaux, with its world-famous wine culture, stunning architecture, and vibrant riverside life.", duration: 1, order: 0, trip: bordeaux_to_berlin)
-file1 = URI.open("https://images.pexels.com/photos/2164683/pexels-photo-2164683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+file2 = URI.open("https://images.pexels.com/photos/2164683/pexels-photo-2164683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
 step1.photo.attach(io: file2, filename: "bordeaux_france.png", content_type: "image/png")
 step1.save
 
 step2 = Step.new(title: "Paris, France", content: "Travel from Bordeaux to the romantic city of Paris, explore iconic landmarks like the Eiffel Tower and Louvre Museum.", duration: 2, order: 0, trip: bordeaux_to_berlin)
-file6 = URI.open("https://media.istockphoto.com/id/1145422105/fr/photo/vue-aérienne-de-la-tour-eiffel-paris.jpg?s=612x612&w=0&k=20&c=uzZ9u2Io4B92oDVeAhyom9zDaMMrdU8HFMl29bV3opU=")
+file6 = URI.open("https://cdn.pixabay.com/photo/2018/04/25/09/26/eiffel-tower-3349075_1280.jpg")
 step2.photo.attach(io: file6, filename: "paris_france.png", content_type: "image/png")
 step2.save
 
@@ -196,7 +183,7 @@ step20.save
 
 
 step21 = Step.new(title: "Lisbon, Portugal", content: "Embark on your journey to Lisbon, explore the city's rich history and enjoy delicious Portuguese pastries.", duration: 9, order: 0, trip: bordeaux_to_valencia)
-file25 = URI.open("https://media.istockphoto.com/id/1483204178/fr/photo/tramway-jaune-typique-à-lisbonne-portugal.jpg?s=612x612&w=0&k=20&c=an28I14QnlwLSm8wdjflujfioIVzUG7MknJn5746ueY=")
+file25 = URI.open("https://img-4.linternaute.com/7GvKgODhSJWxaWCPn72qvLeA4w8=/660x366/smart/5b4ef0d8e6c64a99990e2b6e0e6a7de2/ccmcms-linternaute/10985721.jpg")
 step21.photo.attach(io: file25, filename: "lisbon_portugal.png", content_type: "image/png")
 step21.save
 
@@ -218,38 +205,6 @@ file28 = URI.open("https://media.routard.com/image/70/8/valence-fontaine.1553708
 step24.photo.attach(io: file28, filename: "valencia_spain.png", content_type: "image/png")
 step24.save
 
-
-# step_attributes = [
-  # { title: "Bordeaux, France", content: "Discover the charm of Bordeaux, with its world-famous wine culture, stunning architecture, and vibrant riverside life.", duration: 1, order: 0, trip: bordeaux_to_berlin },
-#   { title: "Paris, France", content: "Travel from Bordeaux to the romantic city of Paris, explore iconic landmarks like the Eiffel Tower and Louvre Museum.", duration: 2, order: 0, trip: bordeaux_to_berlin },
-#   { title: "Brussels, Belgium", content: "Continue your journey to Brussels, enjoy famous Belgian waffles and visit the Grand Place.", duration: 1 , order: 1, trip: bordeaux_to_berlin },
-#   { title: "Amsterdam, Netherlands", content: "Head to Amsterdam, explore the beautiful canals and visit the Van Gogh Museum.", duration: 2 , order: 2, trip: bordeaux_to_berlin },
-#   { title: "Berlin, Germany", content: "Conclude your trip in Berlin, discover historical sites like the Brandenburg Gate and Berlin Wall.", duration: 6, order: 3, trip: bordeaux_to_berlin},
-#   { title: "Bordeaux, France", content: "Discover the charm of Bordeaux, with its world-famous wine culture, stunning architecture, and vibrant riverside life.", duration: 1, order: 0, trip: bordeaux_to_nice },
-#   { title: "Madrid, Spain", content: "Start your adventure by traveling to Madrid, experience vibrant Spanish culture and visit the Prado Museum.", duration: 5, order: 0, trip: bordeaux_to_nice },
-#   { title: "Barcelona, Spain", content: "Proceed to Barcelona, admire Gaudí's architectural masterpieces including Sagrada Família.", duration: 3, order: 1, trip: bordeaux_to_nice },
-#   { title: "Nice, France", content: "Travel to Nice, relax on the French Riviera and stroll along the Promenade des Anglais.", duration: 6, order: 2, trip: bordeaux_to_nice },
-#   { title: "Bordeaux, France", content: "Discover the charm of Bordeaux, with its world-famous wine culture, stunning architecture, and vibrant riverside life.", duration: 1, order: 0, trip: bordeaux_to_prague },
-#   { title: "Geneva, Switzerland", content: "Begin your journey in Geneva, enjoy the stunning Lake Geneva and explore the United Nations headquarters.", duration: 6, order: 0, trip: bordeaux_to_prague },
-#   { title: "Milan, Italy", content: "Continue to Milan, the fashion capital of Italy, and visit the magnificent Duomo di Milano.", duration: 4, order: 1, trip: bordeaux_to_prague },
-#   { title: "Venice, Italy", content: "Head to Venice, experience romantic gondola rides through the city's famous canals.", duration: 2, order: 2, trip: bordeaux_to_prague },
-#   { title: "Vienna, Austria", content: "Conclude your trip in Vienna, explore grand palaces and enjoy classical music concerts.", duration: 6, order: 3, trip: bordeaux_to_prague },
-#   { title: "Prague, Czech Republic", content: "Optional extension to Prague, discover the historic Prague Castle and charming old town.", duration: 3, order: 4, trip: bordeaux_to_prague },
-#   { title: "Bordeaux, France", content: "Discover the charm of Bordeaux, with its world-famous wine culture, stunning architecture, and vibrant riverside life.", duration: 1, order: 0, trip: bordeaux_to_munich },
-#   { title: "Lyon, France", content: "Start by visiting Lyon, known for its culinary excellence and historic architecture.", duration: 5, order: 0, trip: bordeaux_to_munich },
-#   { title: "Zurich, Switzerland", content: "Proceed to Zurich, enjoy beautiful lakeside views and explore Swiss culture.", duration: 4, order: 1, trip: bordeaux_to_munich },
-#   { title: "Munich, Germany", content: "Continue to Munich, visit the historic Marienplatz and enjoy traditional Bavarian cuisine.", duration: 3, order: 2, trip: bordeaux_to_munich },
-#   { title: "Bordeaux, France", content: "Discover the charm of Bordeaux, with its world-famous wine culture, stunning architecture, and vibrant riverside life.", duration: 1, order: 0, trip: bordeaux_to_valencia },
-#   { title: "Lisbon, Portugal", content: "Embark on your journey to Lisbon, explore the city's rich history and enjoy delicious Portuguese pastries.", duration: 9, order: 0, trip: bordeaux_to_valencia },
-#   { title: "Seville, Spain", content: "Travel to Seville, admire the stunning Alcázar palace and experience lively flamenco performances.", duration: 4, order: 1, trip: bordeaux_to_valencia },
-#   { title: "Granada, Spain", content: "Proceed to Granada, visit the breathtaking Alhambra and enjoy panoramic views of the city.", duration: 2, order: 2, trip: bordeaux_to_valencia },
-#   { title: "Valencia, Spain", content: "Conclude your trip in Valencia, relax on beautiful beaches and explore the City of Arts and Sciences.", duration: 5, order: 3, trip: bordeaux_to_valencia }
-# ]
-
-# step_attributes.each do |step_attribute|
-#   step = Step.new(step_attribute)
-#   file = URI.open("")
-# end
 
 puts "#{Step.count} steps created"
 
